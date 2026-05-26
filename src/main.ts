e6a0f8f6e0ccfa1d5b4c3f190d65cb818c694bae
kareemka <92390737+kareemka@users.noreply.github.com>
2026-05-26 18:39:30 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 
-  app.enableCors({
-    origin: [
-      'https://admin.alifadil.com',
-      'https://alifadil.com',
-      'http://localhost:4001',
-      'http://localhost:4002',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'https://admin.alifadil.com',
+  //     'https://alifadil.com',
+  //     'http://localhost:4001',
+  //     'http://localhost:4002',
+  //   ],
+  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  //   credentials: true,
+  // });
 
   await app.listen(4000);
 }
